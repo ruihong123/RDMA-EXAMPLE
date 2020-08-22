a83976ce42b617df11ce4bd4f74e4fbf9e9dbd12
ruihong000 <root@DESKTOP-G267BEM.localdomain>
2020-08-22 19:12:37 -0400

    change lib to cpp

--- 02/build/CMakeFiles/rdma.dir/DependInfo.cmake
+++ 02/build/CMakeFiles/rdma.dir/DependInfo.cmake
@@ -1,15 +1,15 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
+  "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/mnt/c/Users/wangr/Documents/RDMA-EXAMPLE/02/rdma.c" "/mnt/c/Users/wangr/Documents/RDMA-EXAMPLE/02/build/CMakeFiles/rdma.dir/rdma.c.o"
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/wangr/Documents/RDMA-EXAMPLE/02/rdma.cpp" "/mnt/c/Users/wangr/Documents/RDMA-EXAMPLE/02/build/CMakeFiles/rdma.dir/rdma.cpp.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../"
   )
 
--- 02/build/CMakeFiles/rdma.dir/cmake_clean.cmake
+++ 02/build/CMakeFiles/rdma.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/rdma.dir/rdma.c.o"
+  "CMakeFiles/rdma.dir/rdma.cpp.o"
   "librdma.pdb"
   "librdma.so"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang CXX)
   include(CMakeFiles/rdma.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
