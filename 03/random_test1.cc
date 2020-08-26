672e14b6f6f7a5b0f8843acab72302b3e7a5d62a
ruihong000 <root@DESKTOP-G267BEM.localdomain>
2020-08-26 19:35:26 -0400

    cpu cycles

---
+++ 03/random_test1.cc
@@ -0,0 +1,26 @@
+extern "C" {
+#include <get_clock.h>
+}
+#include <iostream>
+#include <cstring>
+//std::chrono::steady_clock::time_point start;
+//std::chrono::steady_clock::time_point end;
+int main(int argc, char* argv[]) {
+
+	double cycles_to_units = get_cpu_mhz(0) * 1000000;
+
+	double format_factor = 0x100000;// : 125000000;
+
+	int msg_size = 1000;
+	char* p1 = new char[msg_size];
+	char* p2 = new char[msg_size];
+	memset(p1, 5, msg_size);
+	memset(p2, 6, msg_size);
+	cycles_t start = get_cycles();
+	memcpy(p2, p1, msg_size);
+	cycles_t end = get_cycles();
+	double bw_avg = end - start;//((double)(msg_size)*cycles_to_units) / ((end - start) * format_factor);
+	std::cout << "memory copy Elapsed time  :"
+		<< bw_avg
+		<< " cycles" << std::endl;
+}
